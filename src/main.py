a89898f14cc6c31ef88b97c1e8ce1421fdaa3051
makhailya <makhailya@gmail.com>
2025-10-08 15:29:54 +0500

    fixis

--- src/main.py
+++ src/main.py
@@ -1,7 +1,3 @@
-from src.file_reader import read_transactions_csv, read_transactions_excel
-from src.formatters import format_transaction
-from src.processing import filter_by_state, sort_by_date
-from src.search import process_bank_search
 from src.utils import read_transactions
 
 
